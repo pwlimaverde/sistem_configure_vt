d19897dc4100991fd2d21d5bfbe5cf5931154a01
pwlimaverde <pwlimaverde@gmail.com>
2024-03-25 16:23:04 -0300

    limpeza packages

--- lib/src/modules/core/core_controller.dart
+++ lib/src/modules/core/core_controller.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:get/get.dart';
 import 'package:logger/logger.dart';
 import 'package:return_success_or_error/return_success_or_error.dart';
@@ -25,21 +24,14 @@ final class CoreController extends GetxController {
       if (value != null) {
         licenca(value['licenca']);
       }
-    readAndWriteFirebaseData();
+      readAndWriteFirebaseData();
     });
-    // _plataformStart();
-    // _plataformStop();
-    // FlutterBackgroundService().invoke('setAsForeground');
+
     // await Workmanager().cancelAll();
-    // licenca.listen((value) async {
-    //   if (value == false) {
-    //     await Workmanager().cancelByTag('1');
-    //     await Workmanager().cancelAll();
-    //   }
-    // });
-    // _registerOneOffTask();
-    // _registerPeriodicTask();
+    _registerPeriodicTask();
   }
+
+  var plataform = const MethodChannel('method.record');
 
   final licenca = Rxn<bool>();
 
@@ -95,20 +87,6 @@ final class CoreController extends GetxController {
     }
   }
 
-  Future<void> _registerOneOffTask() async {
-    await Workmanager().registerOneOffTask(
-      "task1",
-      "OneOffTask",
-      tag: "1",
-      existingWorkPolicy: ExistingWorkPolicy.replace,
-      initialDelay: const Duration(seconds: 3),
-      constraints: Constraints(networkType: NetworkType.connected),
-      backoffPolicy: BackoffPolicy.linear,
-      backoffPolicyDelay: const Duration(seconds: 10),
-      inputData: _config.value,
-    );
-  }
-
   Future<void> _registerPeriodicTask() async {
     await Workmanager().registerPeriodicTask(
       "task2",
@@ -116,7 +94,7 @@ final class CoreController extends GetxController {
       tag: "2",
       frequency: const Duration(minutes: 15),
       existingWorkPolicy: ExistingWorkPolicy.replace,
-      initialDelay: const Duration(seconds: 3),
+      initialDelay: const Duration(seconds: 30),
       constraints: Constraints(networkType: NetworkType.connected),
       backoffPolicy: BackoffPolicy.linear,
       backoffPolicyDelay: const Duration(seconds: 10),
@@ -125,33 +103,26 @@ final class CoreController extends GetxController {
   }
 
   Future<String> plataformInit() async {
-    var plataform = const MethodChannel('method.record');
-    // await Future.delayed(const Duration(seconds: 20));
     final method = await plataform.invokeMethod('onInit');
     Logger().i('MethodChannel result$method');
     return method;
   }
 
   Future<String> plataformStart() async {
-    var plataform = const MethodChannel('method.record');
     final method = await plataform.invokeMethod('onStart');
     Logger().i('MethodChannel result$method');
     return method;
   }
 
   Future<String> plataformStop() async {
-    var plataform = const MethodChannel('method.record');
-    // await Future.delayed(const Duration(seconds: 20));
     final method = await plataform.invokeMethod('onStop');
     Logger().i('MethodChannel result$method');
     return method;
   }
 
-  Future<bool> readAndWriteFirebaseData() async {
+  Future<void> readAndWriteFirebaseData() async {
     final init = await plataformInit();
-          Logger().i('MethodChannel result$init');
-          Logger().i('record start');
-    // Fetch from Firebase
+    Logger().i('MethodChannel result$init');
     Logger().i('firebase conect...');
     final docData =
         FirebaseFirestore.instance.collection("configuracao").doc("options");
@@ -161,24 +132,16 @@ final class CoreController extends GetxController {
     if (snapshot.exists) {
       final int time = snapshot.data()!['time_start'] ?? 15;
       final bool licenca = snapshot.data()!['licenca'] ?? false;
-      if (!licenca) {
-        Logger().i('Service Stoped}');
-      }
       if (licenca) {
         bool repeat = true;
         while (repeat == true) {
-          Logger().i('Service Runn...}');
-          Logger().i('Repeat inicial $repeat');
-
           final start = await plataformStart();
-          Logger().i('MethodChannel result$start');
-          Logger().i('record start');
+          Logger().i('Record start MethodChannel result$start');
 
           await Future.delayed(Duration(minutes: time));
 
           final path = await plataformStop();
           Logger().i('Stop recording $path');
-          Logger().i('Stop recording');
           if (path.isNotEmpty) {
             Logger().i('Inicio Uploade FirebaseStorage');
             await upload(path);
@@ -196,51 +159,17 @@ final class CoreController extends GetxController {
             },
           );
 
-          Logger().i('firebase conect...');
           final docData = FirebaseFirestore.instance
               .collection("configuracao")
               .doc("options");
           final snapshot = await docData.get();
           repeat = snapshot.data()!['licenca'] ?? false;
           if (!repeat) {
-            FlutterBackgroundService().invoke('stopService');
             Logger().i('Service Stoped}');
           }
-          Logger().i('firebase conected - ${snapshot.data()}');
-          Logger().i('Repeat $repeat');
         }
       }
-
-      return licenca;
-    } else {
-      return false;
     }
-  }
-
-  Future<void> record() async {
-    // await Future.delayed(const Duration(seconds: 40));
-    final start = await plataformStart();
-    Logger().i('MethodChannel result$start');
-    Logger().i('record start');
-
-    await Future.delayed(const Duration(seconds: 20));
-
-    final path = await plataformStop();
-    Logger().i('Stop recording $path');
-    if (path.isNotEmpty) {
-      Logger().i('Inicio Uploade FirebaseStorage');
-      await upload(path);
-      Logger().i('Fim Uploade FirebaseStorage');
-    }
-
-    FirebaseFirestore.instance
-        .collection("comandos")
-        .doc("time")
-        .collection("register")
-        .doc()
-        .set({
-      'road': DateTime.now(),
-    });
   }
 
   Future<void> upload(String path) async {
